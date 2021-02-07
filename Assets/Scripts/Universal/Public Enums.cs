43728eb6388873cb10bf3ff08bd217c75acd4b3e
Marc Leino Oconer <sirdoodleystudios@gmail.com>
2021-02-07 19:04:46 +0800

    Updated Numbering of AllCards

--- Assets/Scripts/Universal/Public Enums.cs
+++ Assets/Scripts/Universal/Public Enums.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public enum CombatState { PlayerTurn, CreativeMode, UnleashCreativity, ActiveCard, DrawPahase, EnemyTurn }
 
-public enum CardClass { Neutral, Warrior, Rogue, Mage, Arlen, Cedric, Marie, Luca, Firebrand }
+public enum CardClass { Neutral, Warrior, Rogue, Mage, Arlen, Cedric, Francine, Tilly, Princess }
 
 public enum CardType { Offense, Utility, Ability }
 
@@ -26,37 +26,43 @@ public enum AllJigsaws
 
 
 }
-
-
-//Neu = Neutral = 1XXXX
-//War = Warrior = 2XXXX
-//Rog = Rogue = 3XXXX
-//Mag = Mage = 4XXXX
-//Arl = Arlen = 5XXXX
-//Ced = Cedric = 6XXXX
-//Fra = Francine = 7XXXX
-//Til = Tilly = 8XXXX
-//Pri = Princess = 9XXXX
+//Readcode from right to left
+//XT = Targettable = 1XXXX
+//XD = Dropped =  2XXXX
 //OX = Offensive = X1XXX
 //UX = Utility = X2XXX
 //AX = Ability = X3XXX
-//XT = Targettable = XX1XX
-//XD = Dropped =  XX2XX
-public enum AllCards
+//Neu = Neutral = XX1XX
+//War = Warrior = XX2XX
+//Rog = Rogue = XX3XX
+//Mag = Mage = XX4XX
+//Arl = Arlen = XX5XX
+//Ced = Cedric = XX6XX
+//Fra = Francine = XX7XX
+//Til = Tilly = XX8XX
+//Pri = Princess = XX9XX
+
+//Non-Card 0
+//Neu_OT=10    War_OT=100   Rog_OT=200   Mag_OT=300   Arl_OT=400   Ced_OT=500   Fra_OT=600   Til_OT=700   Pri_OT=800    
+//Neu_OD=900   War_OD=1000  Rog_OD=1100  Mag_OD=1200  Arl_OD=1300  Ced_OD=1400  Fra_OD=1500  Til_OD=1600  Pri_OD=1700    
+//Neu_UT=1800  War_UT=1900  Rog_UT=2000  Mag_UT=2100  Arl_UT=2200  Ced_UT=2300  Fra_UT=2400  Til_UT=2500  Pri_OD=2600
+//Neu_UD=2700  War_UD=2800  Rog_UD=2900  Mag_UD=3000  Arl_UD=3100  Ced_UD=3200  Fra_UD=3300  Til_UD=3400  Pri_UD=3500
+//Neu_AD=3600  War_AD=3700  Rog_AD=3800  Mag_AD=3900  Arl_AD=4000  Ced_AD=4100  Fra_AD=4200  Til_AD=4200  Pri_AD=4300
+
+public enum AllCards: int
 {
     //Non-Cards Classifiers
     Jigsaw = 0,
     Ability,
     //Neutral Offensive Targetted
-    Neu_OT_Attack = 11100,
+    Neu_OT_Attack = 100,
     Neu_OT_Strike,
     //Neutral Utility Dropped
-    Neu_UD_Defend = 12200,
+    Neu_UD_Defend = 2700,
     //Neutral Ability Dropped
-    Neu_AD_Brainstorm = 13200,
-    Neu_AD_Rest,
+    Neu_AD_Brainstorm = 3600,
     //Warrior Utility Dropped
-    War_UD_Reinforce = 22200,
+    War_UD_Reinforce = 2800,
 
 }
 
