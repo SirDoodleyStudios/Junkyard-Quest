432c8f3aff73787e50d428227534bfae16f13ea5
Marc Leino Oconer <sirdoodleystudios@gmail.com>
2021-09-13 23:37:20 +0800

    More Link Activities

---
+++ Assets/Scripts/Link Activities/MovingNode.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class MovingNode : MonoBehaviour
+{
+    RectTransform objectRect;
+    private void Awake()
+    {
+        //tweeing initialize, needs to be called before any DOTween
+        DOTween.Init(true, true, LogBehaviour.Default);
+        DOTween.SetTweensCapacity(20000, 100);
+        objectRect = gameObject.GetComponent<RectTransform>();
+    }
+
+    //called by external script to make the node move
+    public void MovePlayerNode(Vector3 destination)
+    {
+        objectRect.DOAnchorPos(destination, .05f, false);
+    }
+
+}
